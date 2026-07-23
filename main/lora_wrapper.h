cf0d82c62b5b5b1804f6d7e66fc3e33c53ca0917
sajjadrahimi75 <sajjadrahimi192@gamil.com>
2026-07-23 17:22:37 +0200

    Improve SX126x debugging

--- main/lora_wrapper.h
+++ main/lora_wrapper.h
@@ -5,8 +5,26 @@
 extern "C" {
 #endif
 
+/*
+ * Initialize the SX1262 and join
+ * The Things Stack using OTAA.
+ */
 int lora_init(void);
+
+/*
+ * Send one LoRaWAN uplink message.
+ */
 int lora_send(const char *message);
+
+/*
+ * Check for a received LoRaWAN message.
+ *
+ * Downlink support is not implemented yet.
+ */
+int lora_receive(
+    char *received_message,
+    int maximum_length
+);
 
 #ifdef __cplusplus
 }
--- main/main.c
+++ main/main.c
@@ -1,18 +1,67 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include <string.h>
+#include "driver/gpio.h"
 #include "uart_interrupt.h"
 #include "lora_wrapper.h"
-// Test Git update
+
 char SEND_MESSAGE[64]="";
 extern char received_text[64];
 
+/*
+ * Message that we want to send by LoRa.
+ */
+char LORA_MESSAGE[64]="";
+
+/*
+ * Message received from LoRa.
+ */
+char LORA_RECEIVED_MESSAGE[64]="";
+
+/*
+ * 0 = idle
+ * 1 = send LORA_MESSAGE
+ * 2 = message received
+ */
+int LORA_STATUS = 0;
+
+
+
 void app_main(void)
 {
+
+
+    /*
+     * Store the result of LoRa functions.
+     */
+    int lora_result;
+
+    /*
+     * Store the number of received LoRa characters.
+     */
+    int lora_received_length;
+
     uart_interrupt_init();
+
+    /*
+     * Initialize LoRa.
+     */
+    lora_result = lora_init();
+
+    gpio_reset_pin(GPIO_NUM_47);
+    gpio_set_direction(GPIO_NUM_47, GPIO_MODE_OUTPUT);
+    gpio_reset_pin(GPIO_NUM_4);
+    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
+    gpio_reset_pin(GPIO_NUM_7);
+    gpio_set_direction(GPIO_NUM_7,GPIO_MODE_OUTPUT);
+
+    gpio_set_level(GPIO_NUM_47, 1); //LED
+    gpio_set_level(GPIO_NUM_4, 0);  //GPRS ON
+    gpio_set_level(GPIO_NUM_7,0);   //GPRS RESET
 
     while (1)
     {
+        
         /*
          * Send all characters received until now.
          */
@@ -31,10 +80,73 @@ void app_main(void)
         }
 
         /*
+         * LoRa send part.
+         *
+         * When LORA_STATUS is 1,
+         * send the text inside LORA_MESSAGE.
+         */
+                /*
+         * Prepare "Hello" to be sent every second.
+         */
+        
+        strcpy(LORA_MESSAGE, "Hello");
+
+        /*
+         * Request LoRa transmission.
+         */
+        LORA_STATUS = 1;
+
+        if (LORA_STATUS == 1)
+        {
+            /*
+             * Send LORA_MESSAGE to lora_wrapper.cpp.
+             */
+            lora_result = lora_send(LORA_MESSAGE);
+
+            /*
+             * If lora_send returns 0,
+             * sending was successful.
+             */
+            if (lora_result == 0)
+            {
+                /*
+                 * Return LoRa status to idle.
+                 */
+                LORA_STATUS = 0;
+            }
+        }
+
+        /*
+         * LoRa receive part.
+         *
+         * Check whether a LoRa message was received.
+         */
+        lora_received_length = lora_receive(
+            LORA_RECEIVED_MESSAGE,
+            sizeof(LORA_RECEIVED_MESSAGE)
+        );
+
+        /*
+         * If the returned value is bigger than 0,
+         * a message was received.
+         */
+        if (lora_received_length > 0)
+        {
+            /*
+             * Status 2 means a LoRa message
+             * is available inside
+             * LORA_RECEIVED_MESSAGE.
+             */
+            LORA_STATUS = 2;
+        }
+
+        /*
          * Wait one second.
          */
-        vTaskDelay(
-            pdMS_TO_TICKS(1000)
-        );
+        
+         gpio_set_level(GPIO_NUM_47, 1); //LED
+        vTaskDelay(pdMS_TO_TICKS(2000));
+         gpio_set_level(GPIO_NUM_47, 0); //LED
+        vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
